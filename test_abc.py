7c55a2ad88dbe1a35528a1b7f81ba6aae7d55aeb
fangzheng <fz0000@sina.com>
2022-02-20 20:24:49 +0800

    .
    Signed-off-by: fangzheng <fz0000@sina.com>

---
+++ test_abc.py
@@ -0,0 +1,14 @@
+class TestABC:
+
+    def test_01_case1(self):
+        assert True
+
+    def test_02_case2(self):
+        assert False
+
+    def test_03case3(self):
+        assert True
+
+
+if __name__ == '__main__':
+    pass
